dcd4116766e62a04f99ef8382556e2d438975fb0
Simmar Kalsi <simmarkalsi@gmail.com>
2017-12-09 02:18:54 +0530

    Maintenance page edited

--- resources/views/maintenance.blade.php
+++ resources/views/maintenance.blade.php
@@ -9,13 +9,13 @@
         <title>Service Unavailable</title>
 
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,300,600" rel="stylesheet" type="text/css">
 
         <!-- Styles -->
         <style>
             html, body {
                 background-color: #fff;
-                color: #636b6f;
+                color: #fff;
                 font-family: 'Raleway', sans-serif;
                 font-weight: 100;
                 height: 100vh;
@@ -33,7 +33,11 @@
             }
 
             .position-ref {
-                position: relative;
+                margin: 0px;
+                position: absolute;   
+                top: 40%;
+                font-weight: 300;
+                text-shadow: 3px 3px 4px #000000;
             }
 
             .content {
@@ -43,15 +47,43 @@
             .title {
                 font-size: 84px;
             }
+            #particles-js{
+                background-color: #1a82f7;
+                background: url(images/linear_bg_2.png);
+                background-repeat: repeat-x;
+
+                /* Safari 4-5, Chrome 1-9 */
+                background: -webkit-gradient(linear, 0% 0%, 0% 100%, from(#1a82f7), to(#2F2727));
+
+                /* Safari 5.1, Chrome 10+ */
+                background: -webkit-linear-gradient(top, #2F2727, #1a82f7);
+
+                /* Firefox 3.6+ */
+                background: -moz-linear-gradient(top, #2F2727, #1a82f7);
+
+                /* IE 10 */
+                background: -ms-linear-gradient(top, #2F2727, #1a82f7);
+
+                /* Opera 11.10+ */
+                background: -o-linear-gradient(top, #2F2727, #1a82f7);
+            }
         </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            <div class="content">
-                <div class="title">
-                    Be right back.
+        
+            
+                    <div id="particles-js" class="title flex-center full-height  ">
+                        <p class="position-ref">Be right back.</p>
+                    </div>
                 </div>
             </div>
         </div>
+        <script src="https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
+
+        <script>
+            particlesJS.load('particles-js', 'js/particles.json', function(){
+            console.log('particles.json loaded...');
+            });
+        </script>
     </body>
 </html>
