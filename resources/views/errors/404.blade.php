716262bcf147c5a9c77e423752b8492131f6437e
Bhaktij Koli <bhaktijkoli121@gmail.com>
2017-12-25 23:21:21 +0530

    Added 404 error view

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.master')
+@section('pre')
+@php
+$title = "404 Not Found";
+$menu_item = '404';
+@endphp
+@endsection
+@section('content')
+{{-- Start your content here --}}
+@endif
+@endsection
+@section('post')
+@endsection
