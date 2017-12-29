66e60ae3472517d8b884487b0848cd69378fcea0
Bhaktij Koli <bhaktijkoli121@gmail.com>
2017-12-29 18:45:43 +0530

    added staff profile view file

---
+++ resources/views/pages/staff-profile.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.master')
+@section('pre')
+	@php
+	$title = "Profile"
+	$menu_item = 'staff-profile';
+@endphp
+@endsection
+@section('content')
+	<div class="section clearfix object-non-visible" data-animation-effect="fadeIn">
+		<div class="container main-content">
+			{{-- Media body
+			Left side image ( profile photo ) (If possible adjust name and email field below image without label ex-prof.abc <br/><small>emal@gmail.com</small>		)
+			Right side Name, , Email id, designation, qualification(List), Area of intrest(para)
+			Below both ^^
+			experience(list), workshops and training attended(list), publications(list) --}}
+		</div>
+	</div>
+@endsection
+@section('post')
+@endsection
