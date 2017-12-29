3acd0f560c168d4b16d99d7d4f7fc08af667f9bf
Bhaktij Koli <bhaktijkoli121@gmail.com>
2017-12-29 12:32:16 +0530

    Pdf view fix.

--- resources/views/pages/pdfview.blade.php
+++ resources/views/pages/pdfview.blade.php
@@ -5,13 +5,9 @@
 @endphp
 @endsection
 @section('content')
-	<div class="section clearfix object-non-visible" data-animation-effect="fadeIn">
-		<div class="container main-content">
-			<div class="row">
-					<iframe src="http://docs.google.com/viewer?url={{$url}}&embedded=true" width="100%" height="100%" style="border: none;"></iframe>
-			</div>
-		</div>
+	<div style="margin-top:131px">
 	</div>
+	<iframe src="http://docs.google.com/viewer?url={{$url}}&embedded=true" width="100%" height="100%" style="border: none;"></iframe>
 @endsection
 @section('post')
 @endsection
