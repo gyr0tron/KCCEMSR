16e62b1b677d5e5fc1c226800010ad1276b22db2
Bhaktij Koli <bhaktijkoli121@gmail.com>
2018-01-02 21:17:28 +0530

    Pdf view fix

--- resources/views/pages/pdfview.blade.php
+++ resources/views/pages/pdfview.blade.php
@@ -3,11 +3,17 @@
 	@php
 	$menu_item = 'academics';
 @endphp
+<style media="screen">
+#viewer {
+	margin-top: 131px;
+}
+</style>
 @endsection
 @section('content')
-	<div style="margin-top:131px">
-	</div>
-	<iframe src="http://docs.google.com/viewer?url={{$url}}&embedded=true" width="100%" height="100%" style="border: none;"></iframe>
+	<iframe id="viewer" src="http://docs.google.com/viewer?url={{$url}}&embedded=true" width="100%" height="300" style="border: none;"></iframe>
 @endsection
 @section('post')
+	<script type="text/javascript">
+	$('#viewer').height($(document).height() - $('.header').height());
+	</script>
 @endsection
