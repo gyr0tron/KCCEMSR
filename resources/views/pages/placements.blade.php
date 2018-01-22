921f56f08623898eb2a00c835f5a8f689799ff7e
Gyrotron <simmarkalsi@gmail.com>
2018-01-22 13:54:20 +0530

    Added chartjs locally

--- resources/views/pages/placements.blade.php
+++ resources/views/pages/placements.blade.php
@@ -14,7 +14,7 @@
 	</div>
 @endsection
 @section('post')
-	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.min.js"></script>
+	<script type="text/javascript" src="js/chart.min.js"></script>
 	<script>
 	var ctx = document.getElementById("myChart").getContext('2d');
 	var myChart = new Chart(ctx, {
