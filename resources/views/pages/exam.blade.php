1d701c7e09f3715c4e9b0ff5349b0a205f2760ff
Bhaktij Koli <bhaktijkoli121@gmail.com>
2018-01-24 02:09:08 +0530

    Updated exam

--- resources/views/pages/exam.blade.php
+++ resources/views/pages/exam.blade.php
@@ -11,46 +11,23 @@
 				<h1 class="text-center"><strong><span>{{$title}}</span></strong></h1>
 
 				@if ($type == "exam-notices")
-					@foreach (App\FileUpload::where('type',$type)->get() as $fp)
-						<li><i class="fa fa-chevron-right pr-10 text-colored align-marg"></i><a href="{{route('exam-id', [$action, $fp->id])}}">{{$fp->name}}</a></li>
-					@endforeach
+					<ul class="list-unstyled resp-text-sub align-marg">
+						@foreach (App\FileUpload::where('type',$type)->get() as $fp)
+							<li><i class="fa fa-chevron-right pr-10 text-colored align-marg"></i><a href="{{route('exam-id', [$action, $fp->id])}}">{{$fp->name}}</a></li>
+						@endforeach
+					</ul>
 				@else
 					@foreach (App\Department::orderBy('id', 'ASC')->get() as $dep)
 						<h2 class="title text-left resp-text-head align-marg"><strong>{{$dep->name}}</strong></h2>
-						@if ($dep->isPrimary())
-							<h4 class="text-left align-marg">FE</h4>
-							<hr class="align-marg"/>
-							<ul class="list-unstyled resp-text-sub align-marg">
-								@foreach (App\FileUpload::where('type',$type)->where('department',$dep->url)->where('year',"1")->get() as $fp)
-									<li><i class="fa fa-chevron-right pr-10 text-colored align-marg"></i><a href="{{route('exam-id', [$action, $fp->id])}}">{{$fp->name}}</a></li>
-								@endforeach
-							</ul>
-							@continue
-						@endif
-						<h4 class="text-left align-marg">SE</h4>
-						<hr class="align-marg"/>
+						<hr/>
 						<ul class="list-unstyled resp-text-sub align-marg">
-							@foreach (App\FileUpload::where('type',$type)->where('department',$dep->url)->where('year',"2")->get() as $fp)
+							@foreach (App\FileUpload::where('type',$type)->where('department',$dep->url)->orderBy('id', 'DESC')->get() as $fp)
 								<li><i class="fa fa-chevron-right pr-10 text-colored align-marg"></i><a href="{{route('exam-id', [$action, $fp->id])}}">{{$fp->name}}</a></li>
 							@endforeach
 						</ul>
-						<h4 class="text-left align-marg">TE</h4>
-						<hr class="align-marg"/>
-						<ul class="list-unstyled resp-text-sub align-marg">
-							@foreach (App\FileUpload::where('type',$type)->where('department',$dep->url)->where('year','3')->get() as $fp)
-								<li><i class="fa fa-chevron-right pr-10 text-colored align-marg"></i><a href="{{route('exam-id', [$action, $fp->id])}}">{{$fp->name}}</a></li>
-							@endforeach
-						</ul>
-						<h4 class="text-left align-marg">BE</h4>
-						<hr class="align-marg"/>
-						<ul class="list-unstyled resp-text-sub align-marg">
-							@foreach (App\FileUpload::where('type',$type)->where('department',$dep->url)->where('year','4')->get() as $fp)
-								<li><i class="fa fa-chevron-right pr-10 text-colored align-marg"></i><a href="{{route('exam-id', [$action, $fp->id])}}">{{$fp->name}}</a></li>
-							@endforeach
-						</ul>
+						<div class="space"></div>
 					@endforeach
 				@endif
-
 			</div>
 		</div>
 	</div>
