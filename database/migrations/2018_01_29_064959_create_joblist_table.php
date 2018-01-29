e41e96692fa6f491b40dddd55326b9d10634b392
Bhaktij Koli <bhaktijkoli121@outlook.com>
2018-01-29 12:26:14 +0530

    Added joblist database

---
+++ database/migrations/2018_01_29_064959_create_joblist_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateJoblistTable extends Migration
+{
+  /**
+  * Run the migrations.
+  *
+  * @return void
+  */
+  public function up()
+  {
+    Schema::create('joblists', function (Blueprint $table) {
+      $table->increments('id');
+      $table->string('name')->nullable();
+      $table->string('filename')->nullable();
+      $table->integer('created_by');
+      $table->integer('updated_by');
+      $table->timestamps();
+    });
+  }
+
+  /**
+  * Reverse the migrations.
+  *
+  * @return void
+  */
+  public function down()
+  {
+    Schema::dropIfExists('joblists');
+  }
+
+}
