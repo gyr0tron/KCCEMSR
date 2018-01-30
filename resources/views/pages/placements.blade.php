2fc0d6e68bbe96c1eb1f2303f3c3aee0958e1a97
gyr0tron <simmarkalsi@gmail.com>
2018-01-31 00:05:16 +0530

    placements functions and stuff added from spit.ac.in

--- resources/views/pages/placements.blade.php
+++ resources/views/pages/placements.blade.php
@@ -8,7 +8,27 @@
 @section('content')
 	<div id="app" class="section clearfix object-non-visible" data-animation-effect="fadeIn">
 		<div class="container main-content">
-			{{-- <h1 class="title"><h1> --}}
+			<div class="row">
+				<div class="col-md-12">
+					<h1 id="about" class="title text-center"><span>Functions &amp; Responsibilities </span></h1>
+				</div>
+				<div class="col-md-12">
+					<p>Training and Placement Office takes care of the following functions and responsibilities:</p>
+					<div class="col-md-12">
+						<ul class="list-unstyled">
+							<li><i class="fa fa-caret-right pr-10 text-colored list-dept-page-uorder-img"></i> <p class="list-text-ovrflw-fix">To have close liaison with Industry to facilitate Training &amp; Placement activities.</p></li>
+							<li><i class="fa fa-caret-right pr-10 text-colored list-dept-page-uorder-img"></i> <p class="list-text-ovrflw-fix">Perform Training Need Analysis of students after analysing industry needs and skill gaps</p></li>
+							<li><i class="fa fa-caret-right pr-10 text-colored list-dept-page-uorder-img"></i> <p class="list-text-ovrflw-fix">Organize pre-recruitment, technical and soft skill trainings for the second and third year students</p></li>
+							<li><i class="fa fa-caret-right pr-10 text-colored list-dept-page-uorder-img"></i> <p class="list-text-ovrflw-fix">Plan recruitment drives for final year students</p></li>
+							<li><i class="fa fa-caret-right pr-10 text-colored list-dept-page-uorder-img"></i> <p class="list-text-ovrflw-fix">Conduct pre-recruitment trainings by the placed students</p></li>
+							<li><i class="fa fa-caret-right pr-10 text-colored list-dept-page-uorder-img"></i> <p class="list-text-ovrflw-fix">Organize internships and industrial visits for students</p></li>
+							<li><i class="fa fa-caret-right pr-10 text-colored list-dept-page-uorder-img"></i> <p class="list-text-ovrflw-fix">Collect feedback from companies visiting institute for recruitment</p></li>
+							<li><i class="fa fa-caret-right pr-10 text-colored list-dept-page-uorder-img"></i> <p class="list-text-ovrflw-fix">Co-ordinate periodical meetings with students and HOD’s, Principal for feedback on Training &amp; Placement activities</p></li>
+							<li><i class="fa fa-caret-right pr-10 text-colored list-dept-page-uorder-img"></i> <p class="list-text-ovrflw-fix">Prepare schedule, budget, policies for Training &amp; Placement Office.</p></li>
+						</ul>
+					</div>
+				</div>
+			</div>
 			<placement-stats></placement-stats>
 		</div>
 	</div>
