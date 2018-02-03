2c85e1eabb053b5067e5534694cf082c7e477c5c
gyr0tron <simmarkalsi@gmail.com>
2018-02-03 18:39:35 +0530

    Syllabus page added

--- public/js/template.js
+++ public/js/template.js
@@ -391,3 +391,13 @@ $(document).ready(function () {
 		$('.nav-wrapper').toggleClass('show-menu');
 	});
 });
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+	acc[i].onclick = function () {
+		this.classList.toggle("active");
+		this.nextElementSibling.classList.toggle("show");
+	}
+}
--- resources/views/pages/syllabus.blade.php
+++ resources/views/pages/syllabus.blade.php
@@ -8,6 +8,60 @@
 @section('content')
 	<div class="section clearfix object-non-visible" data-animation-effect="fadeIn">
 		<div class="container main-content-sub">
+			<div class="row">
+				<h1 class="text-center"><strong><span>{{$title}}</span></strong></h1>
+				<div class="col-md-12">
+									
+					<button class="accordion active"><strong style="text-transform: none;">Humanities and Applied Sciences</strong></button>
+					<div class="panel show">
+						<ul class="list-unstyled resp-text-sub align-marg">
+							<li><i class="fa fa-chevron-right pr-10 text-colored align-marg"></i><a href="">Syllabus</a></li>
+							<li><i class="fa fa-chevron-right pr-10 text-colored align-marg"></i><a href="">Syllabus</a></li>
+						</ul>
+					</div>
+
+					<button class="accordion"><strong style="text-transform: none;">Computer Engineering</strong></button>
+					<div class="panel">
+						<ul class="list-unstyled resp-text-sub align-marg">
+							<li><i class="fa fa-chevron-right pr-10 text-colored align-marg"></i><a href="">Syllabus</a></li>
+							<li><i class="fa fa-chevron-right pr-10 text-colored align-marg"></i><a href="">Syllabus</a></li>
+						</ul>
+					</div>
+
+					<button class="accordion"><strong style="text-transform: none;">Electronics Engineering</strong></button>
+					<div class="panel">
+						<ul class="list-unstyled resp-text-sub align-marg">
+							<li><i class="fa fa-chevron-right pr-10 text-colored align-marg"></i><a href="">Syllabus</a></li>
+							<li><i class="fa fa-chevron-right pr-10 text-colored align-marg"></i><a href="">Syllabus</a></li>
+						</ul>
+					</div>
+
+					<button class="accordion"><strong style="text-transform: none;">Electronics &amp; Telecommunication</strong></button>
+					<div class="panel">
+						<ul class="list-unstyled resp-text-sub align-marg">
+							<li><i class="fa fa-chevron-right pr-10 text-colored align-marg"></i><a href="">Syllabus</a></li>
+							<li><i class="fa fa-chevron-right pr-10 text-colored align-marg"></i><a href="">Syllabus</a></li>
+						</ul>
+					</div>
+
+					<button class="accordion"><strong style="text-transform: none;">Information Technology</strong></button>
+					<div class="panel">
+						<ul class="list-unstyled resp-text-sub align-marg">
+							<li><i class="fa fa-chevron-right pr-10 text-colored align-marg"></i><a href="">Syllabus</a></li>
+							<li><i class="fa fa-chevron-right pr-10 text-colored align-marg"></i><a href="">Syllabus</a></li>
+						</ul>
+					</div>
+
+					<button class="accordion"><strong style="text-transform: none;">MMS</strong></button>
+					<div class="panel">
+						<ul class="list-unstyled resp-text-sub align-marg">
+							<li><i class="fa fa-chevron-right pr-10 text-colored align-marg"></i><a href="">Syllabus</a></li>
+							<li><i class="fa fa-chevron-right pr-10 text-colored align-marg"></i><a href="">Syllabus</a></li>
+						</ul>
+					</div>
+
+				</div>
+			</div>
 		</div>
 	</div>
 @endsection
