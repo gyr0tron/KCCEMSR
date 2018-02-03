9c4b13e6a54db2386b3cfd6142800db696d74482
Bhaktij Koli <bhaktijkoli121@gmail.com>
2018-02-03 20:41:09 +0530

    Updated maintenance blade

--- resources/views/maintenance.blade.php
+++ resources/views/maintenance.blade.php
@@ -1,82 +1,70 @@
 
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>{{setting('break-title','Service Unavailable')}}</title>
+  <title>{{setting('break-title','Service Unavailable')}}</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,300,600" rel="stylesheet" type="text/css">
+  <!-- Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Raleway:100,300,600" rel="stylesheet" type="text/css">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #fff;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
+  <!-- Styles -->
+  <style>
+  html, body {
+    background-image: url('{{asset('images/bg-image-1.jpg')}}');
+    color: #fff;
+    font-family: 'Raleway', sans-serif;
+    font-weight: 100;
+    height: 100vh;
+    margin: 0;
+  }
 
-            .full-height {
-                height: 100vh;
-            }
+  .full-height {
+    height: 100vh;
+  }
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+  .flex-center {
+    align-items: center;
+    display: flex;
+    justify-content: center;
+  }
 
-            .position-ref {
-                margin: 0px;
-                position: absolute;
-                top: 40%;
-                font-weight: 300;
-                text-shadow: 3px 3px 4px #000000;
-            }
+  .position-ref {
+    margin: 0px;
+    position: absolute;
+    top: 40%;
+    font-size: 1em;
+  }
 
-            .content {
-                text-align: center;
-            }
+  .content {
+    text-align: center;
+  }
 
-            .title {
-                font-size: 84px;
-            }
-            #particles-js{
-                background-color: #1a82f7;
-                background: url(images/linear_bg_2.png);
-                background-repeat: repeat-x;
-
-                /* Safari 4-5, Chrome 1-9 */
-                background: -webkit-gradient(linear, 0% 0%, 0% 100%, from(#1a82f7), to(#2F2727));
-
-                /* Safari 5.1, Chrome 10+ */
-                background: -webkit-linear-gradient(top, #2F2727, #1a82f7);
-
-                /* Firefox 3.6+ */
-                background: -moz-linear-gradient(top, #2F2727, #1a82f7);
-
-                /* IE 10 */
-                background: -ms-linear-gradient(top, #2F2727, #1a82f7);
-
-                /* Opera 11.10+ */
-                background: -o-linear-gradient(top, #2F2727, #1a82f7);
-            }
-        </style>
-    </head>
-    <body>
-        <div id="particles-js" class="title flex-center full-height  ">
-            <p class="position-ref">Be right back.</p>
-        </div>
-        <script src="https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
-        <script>
-            particlesJS.load('particles-js', 'js/particles.json', function(){
-            });
-        </script>
-    </body>
+  .title {
+    font-size: 84px;
+  }
+  #particles-js{
+    background-color: #000000b0;
+  }
+  </style>
+</head>
+<body>
+  <div id="particles-js" class="title flex-center full-height  ">
+    {{-- <p class="position-ref">Be right back.</p> --}}
+    <h1 class="position-ref">We are <span
+      class="txt-rotate"
+      data-period="2000"
+      data-rotate='["simple.", "creative.", "saints of technology!", "developers.", "nerdy." ]'></span>
+    </h1>
+  </div>
+  <script src="https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
+  <script type="text/javascript" src="{{ asset('js/custom.js') }}"></script>
+  <script>
+  particlesJS.load('particles-js', 'js/particles.json', function(){
+  });
+  </script>
+</body>
 </html>
