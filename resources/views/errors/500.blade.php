306627fcda46c9fe10c947a0c8dc2800fb3c62fe
Bhaktij Koli <bhaktijkoli121@gmail.com>
2018-02-05 03:34:28 +0530

    Added error pages.

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -17,9 +17,9 @@
         <div class="col-md-12">
           <div class="error-template">
             <h1>Oops!</h1>
-            <h2>404 Not Found</h2>
+            <h2>Internal Server Error</h2>
             <div class="error-details">
-              Sorry, an error has occured, Requested page not found!
+              The server encountered something unexpected that didn't allow it to complete the request.
             </div>
             <div class="error-actions">
               <a href="/" class="btn btn-primary btn-lg"><span class="glyphicon glyphicon-home"></span>
