9ad85f5aa027192ad8b9393419ae305fe033672c
gyr0tron <simmarkalsi@gmail.com>
2018-02-06 23:11:58 +0530

    Infra blade added carousel

--- resources/views/pages/infrastructure.blade.php
+++ resources/views/pages/infrastructure.blade.php
@@ -15,9 +15,52 @@
 			@for ($i=0; $i < $total; $i++)
 				<div class="section clearfix object-non-visible" data-animation-effect="fadeIn">
 					<div class="row">
-						<div class="col-md-6" {!!$i/2==0?'':'style="float:right"'!!}>
-							<img src="{{$infrastructure[$i]->getUrl()}}"/>
+						<div class="col-md-6" style="float:right">
+							<div class="row" id="slider">
+								<div class="col-md-12">
+									<div id="myCarousel" class="carousel slide">
+										<div class="carousel-inner">
+											@php
+											$no = 0;
+											@endphp
+											@foreach (App\Eventimage::where('event',$event->id)->get() as $image)
+												<div class="item{{$no==0?' active': ''}}" data-slide-number="{{$no++}}">
+													<img src="{{$image->getUrl()}}" width="1200">
+												</div>
+											@endforeach
+										</div>
+
+										<!-- Controls-->
+										<a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+											<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+											<span class="sr-only">Previous</span>
+										</a>
+										<a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+											<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+											<span class="sr-only">Next</span>
+										</a>
+									</div>
+								</div>
+							</div>
+
+							<div class="row">
+								<div class="col-md-12" id="slider-thumbs">
+									<ul class="list-inline">
+										@php
+										$no = 0;
+										@endphp
+										@foreach (App\Eventimage::where('event',$event->id)->get() as $image)
+											<li>
+												<a id="carousel-thumb-{{$no++}}" class="{{$no==0?'selected':''}}">
+													<img src="{{$image->getThumb()}}" width="80" height="60">
+												</a>
+											</li>
+										@endforeach
+									</ul>
+								</div>
+							</div>
 						</div>
+						
 						<div class="col-md-6">
 							<h2 class="title"><span>{{$infrastructure[$i]->name}}</span></h2>
 							{{$infrastructure[$i]->description}}
