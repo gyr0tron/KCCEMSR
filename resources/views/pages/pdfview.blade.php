4668b61fe9988ac4a19264176308659db0155951
gyr0tron <simmarkalsi@gmail.com>
2018-02-07 03:14:04 +0530

    Pdf view added download button

--- resources/views/pages/pdfview.blade.php
+++ resources/views/pages/pdfview.blade.php
@@ -3,16 +3,15 @@
 	@php
 @endphp
 <style media="screen">
-#viewer {
-	margin-top: 131px;
-}
 </style>
 @endsection
 @section('content')
+	<a href="{{$url}}" id="notification"><i class="fa fa-download"></i></a>
 	<iframe id="viewer" src="http://docs.google.com/viewer?url={{$url}}&embedded=true" width="100%" height="300" style="border: none;"></iframe>
 @endsection
 @section('post')
 	<script type="text/javascript">
 	$('#viewer').height($(document).height() - $('.header').height());
+	$("#viewer").css('margin-top', $('.header').height() + 17);
 	</script>
 @endsection
