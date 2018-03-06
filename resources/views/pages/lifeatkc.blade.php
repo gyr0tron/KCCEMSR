eb75ae64bfa88ea76618b21c66149d070dc04848
Bhaktij Koli <bhaktijkoli121@gmail.com>
2018-03-07 00:31:41 +0530

    Fix Life At KC Added All to expection

--- resources/views/pages/lifeatkc.blade.php
+++ resources/views/pages/lifeatkc.blade.php
@@ -25,6 +25,9 @@
 					<!-- portfolio items start -->
 					<div class="isotope-container row grid-space-20">
 						@foreach (App\Event::all() as $event)
+							@php
+							if(!is_in_type($event->department, $types) && $event->department != "all") continue;
+							@endphp
 							<div class="col-sm-6 col-md-3 isotope-item {{$event->department}}">
 								<div class="image-box">
 									<div class="overlay-container">
