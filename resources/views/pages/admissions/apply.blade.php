f848368d63124cfacb69782aa2cda9f85cc7cc76
gyr0tron <simmarkalsi@gmail.com>
2018-03-13 22:56:50 +0530

    Added error messages

--- resources/views/pages/admissions/apply.blade.php
+++ resources/views/pages/admissions/apply.blade.php
@@ -17,40 +17,46 @@
 					</ul>
 
 					<div id="tab-1" class="login tab-content_reg current">
-						<form>
-							<div class="form-section">
+						<form action="">
+							<div class="form-group form-section">
 								<span class="fa fa-envelope-o input-icon"></span>
 								<input type="email" name="name" placeholder="Email">
+								<small class="field-error">The Email is required</small>
 							</div>
-							<div class="form-section">
+							<div class="form-group form-section">
 								<span class="fa fa-unlock-alt input-icon"></span>
 								<input type="password" name="password" placeholder="Password">
+								<small class="field-error">Password is required</small>
 							</div>
-							<div class="form-section btn-container">
+							<div class="form-group form-section btn-container">
 								<input type="submit" value="Login">
 							</div>
 						</form>
 					</div>
 
 					<div id="tab-2" class="register tab-content_reg">
-						<form>
-							<div class="form-section">
+						<form action="">
+							<div class="form-group form-section">
 								<span class="fa fa-user-o input-icon"></span>
 								<input type="text" name="name" placeholder="Name">
+								<small class="field-error">Name is required</small>
 							</div>
-							<div class="form-section">
+							<div class="form-group form-section">
 								<span class="fa fa-envelope-o input-icon"></span>
 								<input type="email" name="name" placeholder="Email" class="email">
+								<small class="field-error">The Email is required</small>
 							</div>
-							<div class="form-section">
+							<div class="form-group form-section">
 								<span class="fa fa-unlock-alt input-icon"></span>
 								<input type="password" name="password" placeholder="Password">
+								<small class="field-error">Password is required</small>
 							</div>
-							<div class="form-section">
+							<div class="form-group form-section">
 								<span class="fa fa-lock input-icon"></span>
 								<input type="password" name="password_cnfrm" placeholder="Confirm Password">
+								<small class="field-error">Passwords do not match!</small>
 							</div>
-							<div class="form-section btn-container">
+							<div class="form-group form-section btn-container">
 								<input type="submit" value="Register">
 							</div>
 						</form>
--- resources/views/pages/grievance.blade.php
+++ resources/views/pages/grievance.blade.php
@@ -10,64 +10,50 @@
 		<div class="container main-content">
 			<h1 class="text-center title"><span>Grievance Redressal</span></h1>
 			<div class="row">
-				<div class="col-md-12">
-					<div class="container">
-						<form class="well form-horizontal" action=" " method="post"  id="job_application">
+				<div class="col-xs-12 col-sm-6 col-md-6 col-md-offset-3  col-sm-offset-3">
+					<form class="well form-horizontal" action=" " method="post"  id="job_application">
 
-							<!-- Text input-->
-							<div class="row">
-								<div class="has-feedback col-md-4 col-md-offset-4" style="margin-bottom: 15px;">
-									<label class="sr-only" for="name">Name</label>
-									<input type="text" class="form-control" id="name" placeholder="Name" name="name">
-									<i class="fa fa-user form-control-feedback"></i>
-								</div>
-							</div>
+						<!-- Text input-->
+						<div class="form-group form-section">
+							<span class="fa fa-user-o input-icon"></span>
+							<input type="text" name="name" placeholder="Name">
+						</div>
 
-							<!-- Text input-->
-							<div class="row">
-								<div class="has-feedback col-md-4 col-md-offset-4" style="margin-bottom: 15px;">
-									<label class="sr-only" for="email">Email address</label>
-									<input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
-									<i class="fa fa-envelope form-control-feedback"></i>
-								</div>
-							</div>
+						<!-- Text input-->
+						<div class="form-group form-section">
+							<span class="fa fa-envelope-o input-icon"></span>
+							<input type="email" name="email" placeholder="Email address" class="email">
+						</div>
 
-							<!-- Text input-->			
-							<div class="row">
-								<div class="has-feedback col-md-4 col-md-offset-4" style="margin-bottom: 15px;">
-									<label class="sr-only" for="phone">Phone No.</label>
-									<input type="text" class="form-control" id="phone" placeholder="Enter Phone No." name="phone">
-									<i class="fa fa-phone form-control-feedback"></i>
-								</div>
-							</div>
+						<!-- Text input-->			
+						<div class="form-group form-section">
+							<span class="fa fa-phone input-icon"></span>
+							<input type="tel" name="phone" placeholder="Phone No.">
+						</div>
 
-							<!-- Text input-->
-							<div class="row">
-								<div class="has-feedback col-md-4 col-md-offset-4" style="margin-bottom: 15px;">
-									<label class="sr-only" for="subject">Subject</label>
-									<input type="text" class="form-control" id="subject" placeholder="Subject" name="subject">
-									<i class="fa fa-quote-left form-control-feedback"></i>
-								</div>
-							</div>
+						<!-- Text input-->
+						<div class="form-group form-section">
+							<span class="fa fa-quote-left input-icon"></span>
+							<input type="text" name="subject" placeholder="Subject">
+						</div>
 
-							<!-- File submit area -->
-							<div class="row">
-								<div class="has-feedback col-md-4 col-md-offset-4" style="margin-bottom: 15px;">
-									<label class="sr-only" for="message">Message</label>
-									<textarea rows="7" class="form-control" placeholder="Message" cols="40" id="message"></textarea>
-									<i class="fa fa-align-left form-control-feedback"></i>
-								</div>
+						<!-- File submit area -->
+						<div class="row">
+							<div class="has-feedback" style="margin-bottom: 15px;">
+								<label class="sr-only" for="message">Message</label>
+								<textarea rows="7" class="form-control" placeholder="Message" cols="40" id="message"></textarea>
+								<i class="fa fa-align-left form-control-feedback"></i>
 							</div>
+						</div>
 
-							<!-- Button -->
-							<div class="form-group">
-								<label class="col-md-4 control-label"></label>
-								<div class="col-md-4">
-									<button type="submit" class="btn btn-primary btn-block btn-full">Send<span class="glyphicon glyphicon-send"></span></button>
-								</div>
+						<!-- Button -->
+						<div class="form-group">
+							<label class="col-md-4 control-label"></label>
+							<div class="col-md-4">
+								<button type="submit" class="btn btn-primary btn-block btn-full">Send<span class="glyphicon glyphicon-send"></span></button>
 							</div>
-						</form>
-					</div>
+						</div>
+					</form>
 				</div>
 			</div>
 		</div>
