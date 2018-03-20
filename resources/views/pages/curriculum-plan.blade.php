957135cfea5219610bbeef2bfcfa8d7fb4134697
gyr0tron <simmarkalsi@gmail.com>
2018-03-21 03:24:23 +0530

    Curriculum plan added

--- resources/views/pages/curriculum-plan.blade.php
+++ resources/views/pages/curriculum-plan.blade.php
@@ -8,6 +8,48 @@
 @section('content')
 	<div id="curriculum-plan" class="section clearfix object-non-visible" data-animation-effect="fadeIn">
 		<div class="container main-content-sub">
+			<h1 class="text-center title"><span>Curriculum Plan</span></h1>
+			<table class="table-fill" style="max-width: max-content;">
+				<thead>
+					<tr>
+						<th>Sr. No</th>
+						<th>Date</th>
+						<th>Activity</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>1</td>
+						<td>07/07/2017</td>
+						<td>Display of the Time Table, Roll list, EXP. List</td>
+					</tr>
+					<tr>
+						<td>1</td>
+						<td>07/07/2017</td>
+						<td>Display of the Time Table, Roll list, EXP. List</td>
+					</tr>
+					<tr>
+						<td>1</td>
+						<td>07/07/2017</td>
+						<td>Display of the Time Table, Roll list, EXP. List</td>
+					</tr>
+					<tr>
+						<td>1</td>
+						<td>07/07/2017</td>
+						<td>Display of the Time Table, Roll list, EXP. List</td>
+					</tr>
+					<tr>
+						<td>1</td>
+						<td>07/07/2017</td>
+						<td>Display of the Time Table, Roll list, EXP. List</td>
+					</tr>
+					<tr>
+						<td>1</td>
+						<td>07/07/2017</td>
+						<td>Display of the Time Table, Roll list, EXP. List</td>
+					</tr>
+				</tbody>
+			</table>
 		</div>
 	</div>
 @endsection
