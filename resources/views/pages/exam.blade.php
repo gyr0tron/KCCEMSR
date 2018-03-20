976fa398ab946055ce9fc00ad8b2558f12a90638
gyr0tron <simmarkalsi@gmail.com>
2018-03-21 02:44:40 +0530

    Fixed exam notices

--- resources/views/pages/exam.blade.php
+++ resources/views/pages/exam.blade.php
@@ -9,20 +9,20 @@
 		<div class="container main-content-sub">
 			<div class="row">
 				<h1 class="text-center"><strong><span>{{$title}}</span></strong></h1>
-
+<hr>
 				@if ($type == "exam-notices")
-					<ul class="list-unstyled resp-text-sub align-marg">
+					<ul class="list-unstyled resp-text-sub" style="margin-left: 12px;">
 						@foreach (App\FileUpload::where('type',$type)->orderBy('created_at', 'DESC')->get() as $fp)
-							<li><i class="fa fa-chevron-right pr-10 text-colored align-marg"></i><a href="{{route('exam-id', [$action, $fp->id])}}">{{$fp->name}}</a><small>&nbsp;&nbsp;{{$fp->created_at->format('d/m/Y')}}</small></li>
+							<li><i style="float: left;margin-top: 7px;" class="fa fa-chevron-right pr-10 text-colored"></i><a style="display: block;" href="{{route('exam-id', [$action, $fp->id])}}"><p style="display:flex;">{{$fp->name}}</p></a><small>&nbsp;&nbsp;{{$fp->created_at->format('d/m/Y')}}</small></li>
 						@endforeach
 					</ul>
 				@else
 					@foreach (App\Department::orderBy('id', 'ASC')->get() as $dep)
-						<h2 class="title text-left resp-text-head align-marg"><strong>{{$dep->name}}</strong></h2>
-						<hr/>
-						<ul class="list-unstyled resp-text-sub align-marg">
+						<h2 class="title text-left resp-text-head"><strong>{{$dep->name}}</strong></h2>
+				<hr>
+						<ul class="list-unstyled resp-text-sub">
 							@foreach (App\FileUpload::where('type',$type)->where('department',$dep->url)->orderBy('id', 'DESC')->get() as $fp)
-								<li><i class="fa fa-chevron-right pr-10 text-colored align-marg"></i><a href="{{route('exam-id', [$action, $fp->id])}}">{{$fp->name}}</a></li>
+								<li><i class="fa fa-chevron-right pr-10 text-colored"></i><a href="{{route('exam-id', [$action, $fp->id])}}">{{$fp->name}}</a></li>
 							@endforeach
 						</ul>
 						<div class="space"></div>
