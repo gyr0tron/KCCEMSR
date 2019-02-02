b13617314a4a995f55f9d4b17b02c11cfdf1a68a
Bhaktij Koli <bhaktijkoli121@gmail.com>
2019-02-02 14:35:58 +0530

    Fix infrastructure

--- app/Infrastructure.php
+++ app/Infrastructure.php
@@ -16,27 +16,29 @@ class Infrastructure extends Model
   ];
   public $timestamps = true;
 
-  public function getFeaturedImage() {
-
-    $idno = $this->images[0];
-    $img = ImageUpload::where("id",$idno)->first();
-    if(!$img) return "http://via.placeholder.com/250x150";
-      return $img->getUrl();
+  public function getFeaturedImage()
+  {
+    if($this->images) {
+      $idno = $this->images[0];
+      $img = ImageUpload::where("id",$idno)->first();
+      if($img) return $img->getUrl();
     }
-    public function uploadImage($file) {
-      $this->checkDirs();
-      $filename = str_random(30) . uniqid() . '.' . 'jpg';
-      $filepath = public_path('public/images/' .$filename);
-      Image::make($file)->save( $filepath );
-      $this->image = $filename;
-    }
-    public function checkDirs() {
-      $path = public_path('public/images');
-      File::isDirectory($path) or File::makeDirectory($path, 0777, true, true);
-    }
-    public function removeImage() {
-      $filename = $this->image;
-      $filepath = public_path('public/images/' .$filename);
-      File::delete($filepath);
-    }
+    return "http://via.placeholder.com/250x150";
   }
+  public function uploadImage($file) {
+    $this->checkDirs();
+    $filename = str_random(30) . uniqid() . '.' . 'jpg';
+    $filepath = public_path('public/images/' .$filename);
+    Image::make($file)->save( $filepath );
+    $this->image = $filename;
+  }
+  public function checkDirs() {
+    $path = public_path('public/images');
+    File::isDirectory($path) or File::makeDirectory($path, 0777, true, true);
+  }
+  public function removeImage() {
+    $filename = $this->image;
+    $filepath = public_path('public/images/' .$filename);
+    File::delete($filepath);
+  }
+}
