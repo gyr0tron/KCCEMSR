5fc746984cbbdcaf0e71158243ca56ed0693c736
Bhaktij Koli <bhaktijkoli121@gmail.com>
2019-02-07 15:07:40 +0530

    Fix infrastructure carouse

--- resources/views/pages/infrastructure.blade.php
+++ resources/views/pages/infrastructure.blade.php
@@ -23,7 +23,7 @@
 						<div class="container" style="float:right">
 							<div class="row" id="slider">
 								<div class="col-md-12">
-									<div id="myCarousel" class="carousel slide">
+									<div id="myCarousel{{$i}}" class="carousel slide">
 										<div class="carousel-inner">
 											@php
 											$no = 0;
@@ -39,11 +39,11 @@
 										</div>
 
 										<!-- Controls-->
-										<a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+										<a class="left carousel-control" href="#myCarousel{{$i}}" role="button" data-slide="prev">
 											<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
 											<span class="sr-only">Previous</span>
 										</a>
-										<a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+										<a class="right carousel-control" href="#myCarousel{{$i}}" role="button" data-slide="next">
 											<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
 											<span class="sr-only">Next</span>
 										</a>
