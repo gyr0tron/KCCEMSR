5eb128230056bf0c0ae26ad31e8b453cd876c60d
Bhaktij Koli <bhaktijkoli121@gmail.com>
2019-10-04 00:00:02 +0530

    Added hvecell page

--- resources/views/pages/hvecell/index.blade.php
+++ resources/views/pages/hvecell/index.blade.php
@@ -9,7 +9,6 @@
 	<div class="section clearfix object-non-visible" data-animation-effect="fadeIn">
 		<div class="container main-content-sub tab-pane fade in">
 			<h1 class="text-center title"><span>Human Values Education Cell</span></h1>
-
 			<div class="text-center">
 				<p>Coming soon..</p>
 			</div>
